70680b4606ea4c7730545abef1f95148a7a703d2
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-08-20 20:52:05 +0200

    useAuthState

--- pages/admin.js
+++ pages/admin.js
@@ -5,6 +5,7 @@ import Login from "../components/login";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const auth = firebase.auth();
 
