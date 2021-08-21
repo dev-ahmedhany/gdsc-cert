91b0743add6bb3ac9a5b434c094383bf57be4c85
Ahmed Hany <dev.ahmedhany@gmail.com>
2021-08-21 09:50:29 +0200

    generate images

--- pages/c/[id].js
+++ pages/c/[id].js
@@ -2,6 +2,9 @@ import firebase from "firebase";
 import fs from "fs";
 import path from "path";
 import Cert from "../../components/cert";
+import GDSCCoreTeamCertification2021 from "../../components/cert/GDSCCoreTeamCertification2021";
+import ReactDOMServer from 'react-dom/server';
+const sharp = require("sharp")
 
 export default function C(props) {
   return <Cert {...props}></Cert>;
@@ -12,47 +15,36 @@ export async function getStaticProps(context) {
   const data = JSON.parse(fs.readFileSync(path.resolve("data.json")));
   const info = data.find((d) => d.id === id);
 
-  if (info) {
-    return { props: info };
-  } else {
-    const db = firebase.firestore();
-
-    var docRef = db
-      .collection("cert")
-      .doc(id.substring(0, 2))
-      .collection("core21")
-      .doc(id);
-
-    try {
-      const doc = await docRef.get();
-      if (doc?.exists) {
-        return { props: doc.data() };
-      } else {
-        // doc.data() will be undefined in this case
-        return { props: { error: "Not Found" } };
-      }
-    } catch (error) {
-      console.log("Error getting document:", error);
-    }
-  }
-
-  return {
-    props: {}, // will be passed to the page component as props
-  };
+  const roundedCorners = Buffer.from(ReactDOMServer.renderToStaticMarkup(<GDSCCoreTeamCertification2021 {...info} />))
+  sharp(roundedCorners)
+  .png()
+  .toFile(`${path.resolve("public/c/",id)}.png`)
+  .catch(function(err) {
+    console.log(err)
+  })
+  return { props: info };
 }
 
 export async function getStaticPaths() {
+  var dir = 'public/c/';
+
+if (!fs.existsSync(path.resolve(dir))){ 
+    fs.mkdirSync(path.resolve(dir));
+}
+
   const data = [];
   const paths = [];
   try {
     const db = firebase.firestore();
-
     var docRef = db.collectionGroup("core21");
     const querySnapshot = await docRef.get();
     querySnapshot.forEach((doc) => {
       paths.push({ params: { id: doc.id } });
       data.push({ id: doc.id, ...doc.data() });
     });
+    if(paths.length === 0){
+      throw "empty list"
+    }
     fs.writeFileSync(path.resolve("data.json"), JSON.stringify(data));
   } catch (error) {
     console.log("Error getting document:", error);
