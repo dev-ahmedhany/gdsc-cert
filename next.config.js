d5faef633191525849b35da3a64f1b86cd9abb77
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2021-12-16 18:03:43 +0200

    fix fallback

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
 module.exports = {
   reactStrictMode: true,
   async rewrites() {
-    return [
-      {
-        source: "/ahmed",
-        destination: "/admin",
-      },
-      {
-        source: "/preview",
-        destination: "/admin",
-      },
-      {
-        source: "/c/:id",
-        destination: "/cert",
-      },
-      {
-        source: "/validate/:id",
-        destination: "/validate",
-      },
-    ];
+    return {
+      fallback:[
+        {
+          source: "/ahmed",
+          destination: "/admin",
+        },
+        {
+          source: "/preview",
+          destination: "/admin",
+        },
+        {
+          source: "/c/:id",
+          destination: "/cert",
+        },
+        {
+          source: "/validate/:id",
+          destination: "/validate",
+        },
+      ]
+    };
   },
 };
