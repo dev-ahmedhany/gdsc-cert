dfb1e7c0c5b6b65074f3b872ca0573075412aded
Ahmed Hany <dev.ahmedhany@gmail.com>
2022-03-03 10:53:15 +0200

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -1,21 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: "/ahmed",
-          destination: "/admin",
-        },
-        {
-          source: "/preview",
-          destination: "/admin",
-        },
-        {
-          source: "/validate/:id",
-          destination: "/validate",
-        },
-      ],
-    };
-  },
 };
